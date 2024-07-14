5c1ad2311ab8a2b7d0c25e3dafbd6f246a2773c4
Alex Dubrovin <legyssss@gmail.com>
2024-07-14 17:28:45 +0300

    rm rq

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React, { ReactNode } from "react";
-import ReactQueryProvider from "@/providers/ReactQueryProvider/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
